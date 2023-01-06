825425ddb3e82a16135f0afae76fdbded63c7d4f
Nick Dillon <nickhds@gmail.com>
2023-01-05 19:17:44 -0600

    Changed images path

--- src/components/Technologies.vue
+++ src/components/Technologies.vue
@@ -4,7 +4,7 @@
       <li v-for="technology in technologies" :key="technology.title" class="hover:bg-gray-100 hover:rounded-lg p-2 w-36">
         <a :href="technology.url" target="_blank">
           <div class="flex justify-center mb-2">
-            <img :src="technology.image" :alt="technology.title" />
+            <img :src="'../../src/assets/images/' + technology.image" :alt="technology.title" />
           </div>
           <div class="text-center">
             <h1 class="font-semibold text-xl">{{ technology.title }}</h1>
@@ -21,22 +21,22 @@ import { ref } from 'vue';
 const technologies = ref([
   {
     url: 'https://laravel.com',
-    image: '../../src/assets/images/laravel.svg',
+    image: 'laravel.svg',
     title: 'Laravel'
   },
   {
     url: 'https://vuejs.org/',
-    image: '../../src/assets/images/vue.svg',
+    image: 'vue.svg',
     title: 'Vue.js'
   },
   {
     url: 'https://tailwindcss.com/',
-    image: '../../src/assets/images/tailwindcss.svg',
+    image: 'tailwindcss.svg',
     title: 'Tailwind CSS'
   },
   {
     url: 'https://inertiajs.com/',
-    image: '../../src/assets/images/inertia.svg',
+    image: 'inertia.svg',
     title: 'Inertia.js'
   },
 ]);
