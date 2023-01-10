982cc6617287c5c4696f1c5690ba4052e16f46aa
Nick Dillon <nickhds@gmail.com>
2023-01-10 16:20:13 -0600

    Fixed Socials styling issue and HomeView top margin

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="text-gray-800">
-    <div class="flex items-center justify-center mx-auto mt-10 w-11/12">
+    <div class="flex items-center justify-center mx-auto mt-1 md:mt-8 w-11/12">
       <div class="flex flex-col md:flex-row items-center sm:justify-between">
         <div class="flex flex-col px-4 mb-4 max-w-lg">
           <h1 class="font-bold text-4xl mb-5 text-center sm:text-left">A little about me...</h1>
           <p class="font-semibold text-xl sm:mr-8 sm:pl-0">
             Hey! I'm Nick, and I'm a full stack developer. I love creating web applications, solving real-world problems, and learning new things every day. I'm also a huge fan of coffee, I love music, and I play the drums.
           </p>
-          <Socials class="mt-5 mb-5 pl-3 sm:pl-0" />
+          <Socials class="mt-5 mb-5 pl-0" />
         </div>
         <img class="rounded-full w-72 h-72" src="/images/profile.jpg" alt="Profile Image">
       </div>
