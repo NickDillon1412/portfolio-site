66d33398b990289b15d50df3ec8a944f76440ef3
Nick Dillon <nickhds@gmail.com>
2023-06-17 22:16:32 -0500

    Updated background colors

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex space-x-3">
-    <div class="pb-1 ml-0 border-b-2 border-transparent hover:border-b-2 hover:border-red-400">
+    <div class="pb-1 ml-0 border-b-2 border-transparent hover:border-b-2 hover:border-[#38eba4]">
       <a
         href="https://twitter.com/NickDillon1412"
         target="_blank"
       >
-        <TwitterIcon class="text-[#1d9bf0]" width="40px" height="40px" />
+        <TwitterIcon class="text-slate-100" width="40px" height="40px" />
       </a>
     </div>
-    <div class="pb-1 ml-0 border-b-2 border-transparent hover:border-b-2 hover:border-red-400">
+    <div class="pb-1 ml-0 border-b-2 border-transparent hover:border-b-2 hover:border-[#38eba4]">
       <a
         href="https://github.com/NickDillon1412"
         target="_blank"
@@ -16,12 +16,12 @@
         <GitHubIcon class="text-slate-100" width="40px" height="40px" />
       </a>
     </div>
-    <div class="pb-1 ml-0 border-b-2 border-transparent hover:border-b-2 hover:border-red-400">
+    <div class="pb-1 ml-0 border-b-2 border-transparent hover:border-b-2 hover:border-[#38eba4]">
       <a
         href="https://www.linkedin.com/in/nickdillon12/"
         target="_blank"
       >
-        <LinkedInIcon class="text-[#0b66c3]" width="40px" height="40px" />
+        <LinkedInIcon class="text-slate-100" width="40px" height="40px" />
       </a>
     </div>
   </div>
