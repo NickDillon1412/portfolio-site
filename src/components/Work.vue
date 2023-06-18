aad6dbab23af8d95c6a04fd805fb0564cb6de9e4
Nick Dillon <nickhds@gmail.com>
2023-06-18 01:09:53 -0500

    Create Work.vue

---
+++ src/components/Work.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="w-10/12 p-10 rounded-md">
+        <ol class="relative border-l border-gray-200 space-y-14 dark:border-gray-700">                  
+            <li class="ml-6">            
+                <span class="absolute flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full -left-4 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-950">
+                    <img class="shadow-lg" src="../../public/images/gray-logo.png" alt="Gray Logo"/>
+                </span>
+                <div class="items-center justify-between ml-2 shadow-sm sm:flex">
+                    <time class="mb-1 text-xs font-normal text-slate-100 sm:order-last sm:mb-0 whitespace-nowrap">April 2023</time>
+                    <div class="space-y-1 text-sm text-gray-100">
+                        <h1 class="text-xl font-semibold">Gray Television</h1>
+                        <h2 class="text-sm">Digital Applications Developer</h2>
+                    </div>
+                </div>
+            </li>
+            <li class="ml-6">
+                <span class="absolute flex items-center justify-center w-8 h-8 bg-blue-100 rounded-full -left-4 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-950">
+                    <img class="shadow-lg" src="../../public/images/buildonline-logo.png" alt="Build Online Logo"/>
+                </span>
+                <div class="items-center justify-between ml-2 shadow-sm sm:flex">
+                    <time class="mb-1 text-xs font-normal text-slate-100 sm:order-last sm:mb-0 whitespace-nowrap">Summer 2021</time>
+                    <div class="space-y-1 text-sm text-gray-100">
+                        <h1 class="text-xl font-semibold">Build Online</h1>
+                        <h2 class="text-sm">Full Stack Developer</h2>
+                    </div>
+                </div>
+            </li>
+        </ol>
+
+        <!-- <ol class="relative p-4 border-l border-gray-200 dark:border-gray-700">                  
+            <li class="mb-10 ml-4">
+                <div class="flex items-center">
+                    <div class="ml-2.5">
+                        <div class="absolute w-40 h-40 rounded-full mt-1.5 -left-5"><img src="../../public/images/gray-logo.png" height="40" width="40" alt="Gray Logo" /></div>
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Gray Television</h3>
+                        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Digital Applications Developer</p>
+                        <time class="mb-1 text-sm font-normal leading-none text-gray-400">April 2023 - Present</time>
+                    </div>
+                </div>
+            </li>
+            <li class="mb-10 ml-4">
+                <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Summer 2021</time>
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Build Online</h3>
+                <p class="text-base font-normal text-gray-500 dark:text-gray-400">Full Stack Developer</p>
+            </li>
+        </ol> -->
+
+        <!-- <div class="py-6">
+            <div class="space-y-3">
+                <div class="flex items-center">
+                    <img src="../../public/images/gray-logo.png" height="40" width="40" alt="Gray Logo" />
+                    <div class="ml-2.5">
+                        <h1 class="text-2xl font-semibold">Gray Television</h1>
+                        <div class="flex items-center space-x-16">
+                            <h2 class="text-sm">Digital Applications Developer</h2>
+                            <h3 class="text-sm">April 2023 - Present</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-col justify-center p-1 mx-auto space-y-2">
+                </div>
+            </div>
+        </div> -->
+        <!-- <div class="border-t w-full max-w-[450px] mx-auto"></div> -->
+        <!-- <div class="py-6">
+            <div class="space-y-3">
+                <div class="flex items-center">
+                    <img src="../../public/images/buildonline-logo.png" height="40" width="40" alt="Build Online Logo" />
+                    <div class="ml-2.5">
+                        <h1 class="text-2xl font-semibold">Build Online</h1>
+                        <div class="flex items-center space-x-16">
+                            <h2 class="text-sm">Full Stack Developer</h2>
+                            <h3 class="text-sm">Summer 2021</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-1 space-y-2">
+                </div>
+            </div>
+        </div> -->
+    </div>
+</template>
