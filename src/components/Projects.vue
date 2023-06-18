a63d73a19b8eb06fa063635720d15d407c649055
Nick Dillon <nickhds@gmail.com>
2023-06-18 18:08:07 -0500

    Updated projects to loop

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,45 +1,51 @@
 <template>
-  <div class="flex flex-col items-center justify-center mx-auto sm:flex-row">
+  <div class="sm:flex-row">
     <div class="grid grid-cols-1 gap-2 mt-6 mb-6 sm:grid-cols-2 gap-y-5">
-      <div class="mx-auto text-lg font-semibold border cursor-pointer sm:w-10/12 text-gray-50 bg-[#363d68] rounded-md border-[#363d68] hover:border-[#38eba4]">
-          <a href="https://github.com/NickDillon1412/Contacts" target="_blank">
-              <div>
-                  <img class="object-cover w-full h-64 sm:h-64 rounded-t-md" src="/images/contacts.png" alt="Contacts" />
-              </div>
-          </a>
-          <div class="p-3 bg-[#363d68] rounded-b-md">
-            <h1>Contacts App</h1>
-            <div>
-                <ul class="flex flex-row flex-wrap items-center justify-start gap-2 pt-3 text-xs">
-                  <li class="p-1 border border-white rounded">Vue.js</li>
-                  <li class="p-1 border border-white rounded">Laravel</li>
-                  <li class="p-1 border border-white rounded">Inertia.js</li>
-                  <li class="p-1 border border-white rounded">Tailwind</li>
-                </ul>
-            </div>
+      <div v-for="project in projects" :key="project.title" class="mx-auto text-lg font-semibold border cursor-pointer sm:w-10/12 text-gray-50 bg-[#363d68] rounded-md border-[#363d68] hover:border-slate-200">
+        <a :href="project.link">
+          <div>
+            <img class="object-cover w-full h-64 sm:h-64 rounded-t-md" :src="`../images/${project.image}`" :alt="project.title" />
           </div>
-      </div>
-
-      <div class="mx-auto text-lg font-semibold border cursor-pointer sm:w-10/12 text-gray-50 bg-[#363d68] rounded-md border-[#363d68] hover:border-[#38eba4]">
-          <a href="https://github.com/NickDillon1412/Recipe-App" target="_blank">
-              <div class="">
-                  <img class="items-center object-cover w-full h-64 sm:h-64 rounded-t-md" src="/images/recipes.png" alt="Recipes" />
-              </div>
-          </a>
-          <div class="p-3 bg-[#363d68] rounded-b-md">
-            <h1>Recipes App</h1>
-            <div>
-                <ul class="flex flex-row flex-wrap items-center justify-start gap-2 pt-3 text-xs">
-                  <li class="p-1 border border-white rounded">Laravel</li>
-                  <li class="p-1 border border-white rounded">Livewire</li>
-                  <li class="p-1 border border-white rounded">Alpine.js</li>
-                  <li class="p-1 border border-white rounded">Tailwind</li>
-                </ul>
-            </div>
+        </a>
+        <div class="p-3 bg-[#363d68] rounded-b-md">
+          <h1>{{ project.title }}</h1>
+          <div>
+            <ul v-for="tech in project.technologies" class="inline-block pt-3 text-xs">
+              <li class="p-1 mr-2 border border-white rounded">{{ tech }}</li>
+            </ul>
           </div>
         </div>
+      </div>
     </div>
   </div>
 </template>
 
+<script setup>
+import { ref } from 'vue';
+
+const projects = ref({
+  contacts: {
+    link: 'https://github.com/NickDillon1412/Contacts',
+    image: 'contacts.png',
+    title: 'Contacts App',
+    technologies: [
+      'Vue.js',
+      'Inertia.js',
+      'Laravel',
+      'Tailwind'
+    ]
+  },
+  recipes: {
+    link: 'https://github.com/NickDillon1412/Recipe-App',
+    image: 'recipes.png',
+    title: 'Recipes App',
+    technologies: [
+      'Tailwind',
+      'Alpine.js',
+      'Laravel',
+      'Livewire'
+    ]
+  }
+});
+</script>
   
