2397c4139aa5742276b404076bc38369500c68e0
Nick Dillon <nickhds@gmail.com>
2023-06-18 18:18:23 -0500

    Updated link to url

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,7 +2,7 @@
   <div class="sm:flex-row">
     <div class="grid grid-cols-1 gap-2 mt-6 mb-6 sm:grid-cols-2 gap-y-5">
       <div v-for="project in projects" :key="project.title" class="mx-auto text-lg font-semibold border cursor-pointer sm:w-10/12 text-gray-50 bg-[#363d68] rounded-md border-[#363d68] hover:border-slate-200">
-        <a :href="project.link">
+        <a :href="project.url">
           <div>
             <img class="object-cover w-full h-64 sm:h-64 rounded-t-md" :src="`../images/${project.image}`" :alt="project.title" />
           </div>
@@ -25,7 +25,7 @@ import { ref } from 'vue';
 
 const projects = ref({
   contacts: {
-    link: 'https://github.com/NickDillon1412/Contacts',
+    url: 'https://github.com/NickDillon1412/Contacts',
     image: 'contacts.png',
     title: 'Contacts App',
     technologies: [
@@ -36,7 +36,7 @@ const projects = ref({
     ]
   },
   recipes: {
-    link: 'https://github.com/NickDillon1412/Recipe-App',
+    url: 'https://github.com/NickDillon1412/Recipe-App',
     image: 'recipes.png',
     title: 'Recipes App',
     technologies: [
