4e87084d2ebedf634d3751e0ed1cf7e17e36c810
Nick Dillon <nickhds@gmail.com>
2023-06-18 01:09:38 -0500

    Updated styling

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center mx-auto sm:flex-row">
     <div class="grid grid-cols-1 gap-2 mt-6 mb-6 sm:grid-cols-2 gap-y-5">
-      <div class="mx-auto text-lg font-semibold border-4 cursor-pointer sm:w-10/12 text-gray-50 bg-slate-800 rounded-xl border-[#363d68] hover:border-[#38eba4]">
+      <div class="mx-auto text-lg font-semibold border cursor-pointer sm:w-10/12 text-gray-50 bg-[#363d68] rounded-md border-[#363d68] hover:border-[#38eba4]">
           <a href="https://github.com/NickDillon1412/Contacts" target="_blank">
               <div>
                   <img class="object-cover w-full h-64 sm:h-64 rounded-t-md" src="/images/contacts.png" alt="Contacts" />
@@ -11,16 +11,16 @@
             <h1>Contacts App</h1>
             <div>
                 <ul class="flex flex-row flex-wrap items-center justify-start gap-2 pt-3 text-xs">
-                  <li class="p-1 border-2 border-white rounded-md">Vue.js</li>
-                  <li class="p-1 border-2 border-white rounded-md">Laravel</li>
-                  <li class="p-1 border-2 border-white rounded-md">Inertia.js</li>
-                  <li class="p-1 border-2 border-white rounded-md">Tailwind</li>
+                  <li class="p-1 border border-white rounded">Vue.js</li>
+                  <li class="p-1 border border-white rounded">Laravel</li>
+                  <li class="p-1 border border-white rounded">Inertia.js</li>
+                  <li class="p-1 border border-white rounded">Tailwind</li>
                 </ul>
             </div>
           </div>
       </div>
 
-      <div class="mx-auto text-lg font-semibold border-4 cursor-pointer sm:w-10/12 text-gray-50 bg-slate-800 rounded-xl border-[#363d68] hover:border-[#38eba4]">
+      <div class="mx-auto text-lg font-semibold border cursor-pointer sm:w-10/12 text-gray-50 bg-[#363d68] rounded-md border-[#363d68] hover:border-[#38eba4]">
           <a href="https://github.com/NickDillon1412/Recipe-App" target="_blank">
               <div class="">
                   <img class="items-center object-cover w-full h-64 sm:h-64 rounded-t-md" src="/images/recipes.png" alt="Recipes" />
@@ -30,10 +30,10 @@
             <h1>Recipes App</h1>
             <div>
                 <ul class="flex flex-row flex-wrap items-center justify-start gap-2 pt-3 text-xs">
-                  <li class="p-1 border-2 border-white rounded-md">Laravel</li>
-                  <li class="p-1 border-2 border-white rounded-md">Livewire</li>
-                  <li class="p-1 border-2 border-white rounded-md">Alpine.js</li>
-                  <li class="p-1 border-2 border-white rounded-md">Tailwind</li>
+                  <li class="p-1 border border-white rounded">Laravel</li>
+                  <li class="p-1 border border-white rounded">Livewire</li>
+                  <li class="p-1 border border-white rounded">Alpine.js</li>
+                  <li class="p-1 border border-white rounded">Tailwind</li>
                 </ul>
             </div>
           </div>
