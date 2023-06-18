b4e452f0ec35d493eda277304bf0cd6354f02175
Nick Dillon <nickhds@gmail.com>
2023-06-17 21:58:51 -0500

    Updated styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="text-gray-800">
-    <div class="flex items-center justify-center mx-auto mt-1 md:mt-8 w-11/12">
-      <div class="flex flex-col md:flex-row items-center sm:justify-between">
-        <div class="flex flex-col px-4 mb-4 max-w-lg">
-          <h1 class="font-bold text-4xl mb-5 text-center sm:text-left">A little about me...</h1>
-          <p class="font-semibold text-xl sm:mr-8 sm:pl-0">
+  <div class="text-slate-100">
+    <div class="flex items-center justify-center w-11/12 mx-auto mt-1 md:mt-8">
+      <div class="flex flex-col items-center md:flex-row sm:justify-between">
+        <div class="flex flex-col max-w-lg px-4 mb-4">
+          <h1 class="mb-5 text-4xl font-bold text-center sm:text-left">A little about me...</h1>
+          <p class="text-xl font-semibold sm:mr-8 sm:pl-0">
             Hey! I'm Nick, and I'm a full stack developer. I love creating web applications, solving real-world problems, and learning new things every day. I'm also a huge fan of coffee, I love music, and I play the drums.
           </p>
-          <Socials class="mt-5 mb-5 pl-0" />
+          <Socials class="pl-0 mt-5 mb-5" />
         </div>
         <img class="rounded-full w-72 h-72" src="/images/profile.jpg" alt="Profile Image">
       </div>
     </div>
 
-    <div class="flex flex-col items-center justify-center mt-24 sm:mt-32">
-      <h1 class="text-3xl font-semibold w-9/12 text-center mb-3">Technologies I use on a daily basis:</h1>
+    <div class="flex flex-col items-center justify-center mt-24 sm:mt-32 text-slate-100">
+      <h1 class="w-9/12 mb-3 text-3xl font-semibold text-center">Technologies I use on a daily basis:</h1>
       <Technologies />
     </div>
 
     <div class="flex flex-col items-center justify-center mt-24 sm:mt-32 mb-14">
       <div class="p-4">
-        <h1 class="text-3xl font-semibold text-center w-9/12 mb-3 mx-auto">A couple of my projects:</h1>
+        <h1 class="w-9/12 mx-auto mb-3 text-3xl font-semibold text-center">A couple of my projects:</h1>
         <Projects class="pl-3 pr-3" />
       </div>
     </div>
