f8b17e8c5a3291a52ce6e4f818ae70bf96857978
Nick Dillon <nickhds@gmail.com>
2023-08-22 18:15:39 -0500

    Changed nav spacing

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
     <nav
-        class="fixed z-10 flex items-center justify-between w-full px-3.5 py-3 sm:py-5 border-b sm:px-10 border-slate-700 backdrop-blur-2xl text-slate-50">
+        class="fixed z-10 flex items-center justify-between w-full px-3.5 py-3 sm:py-3.5 border-b sm:px-10 border-slate-700 backdrop-blur-2xl text-slate-50">
         <RouterLink to="/" name="Home">
             <h1
                 class="text-3xl font-semibold tracking-tighter uppercase duration-300 ease-in-out hover:text-pink-400 hover:scale-110">
