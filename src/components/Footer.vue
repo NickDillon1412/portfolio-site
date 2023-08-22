d1f3560bf60d38d79f68d7dca43578a53f4daa2f
Nick Dillon <nickhds@gmail.com>
2023-08-22 11:04:32 -0500

    Added pulse effect

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,12 +13,12 @@
                             class="relative sm:w-auto w-full hover:-rotate-3 transition-all ease-out duration-300 inline-flex items-center justify-center px-4 py-2 text-lg font-bold text-white bg-pink-500 hover:bg-pink-500 hover:scale-[1.01] rounded-xl "
                             role="button">Send me a message!</a>
                     </div> -->
-                    <div class="relative inline-flex w-full group">
+                    <div class="relative inline-flex w-full group/button">
                         <div
-                            class="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-pink-500 to-pink-500 rounded-xl blur-2xl group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+                            class="absolute transition-all duration-1000 bg-pink-500 -inset-px rounded-xl blur-lg group-hover/button:opacity-100 group-hover/button:-inset-1 group-hover/button:duration-200 animate-tilt">
                         </div>
                         <a href="https://github.com/laravel-frontend-presets/tall" target="_blank"
-                            class="relative sm:w-auto w-full hover:-rotate-3 transition-all ease-out duration-300 inline-flex items-center justify-center px-4 py-2 text-lg font-bold text-white bg-pink-500 hover:scale-[1.01] rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+                            class="relative inline-flex items-center justify-center w-full px-4 py-2 text-lg font-bold text-white transition-all duration-300 ease-out bg-pink-500 sm:w-auto hover:-rotate-3 hover:scale-110 rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
                             role="button">Send me a message!</a>
                     </div>
                     <span class="hidden sm:inline-block">|</span>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,24 @@
 <template>
     <nav
         class="fixed z-10 flex items-center justify-between w-full px-3.5 py-3 sm:py-5 border-b sm:px-10 border-slate-700 backdrop-blur-xl text-slate-50">
-        <RouterLink to="/" name="Home">
+        <!-- <RouterLink to="/" name="Home">
             <h1 class="text-3xl font-semibold tracking-tighter uppercase duration-200 ease-in-out hover:text-pink-400">
                 Nick
                 Dillon</h1>
-        </RouterLink>
+        </RouterLink> -->
+
+        <div class="relative inline-flex group/button">
+            <div
+                class="absolute transition-all duration-1000 bg-pink-500 opacity-90 -inset-px rounded-xl blur-xl group-hover/button:opacity-100 group-hover/button:-inset-1 group-hover/button:duration-200 animate-pulse">
+            </div>
+            <RouterLink to="/" name="Home"
+                class="relative inline-flex items-center justify-center w-full px-4 text-lg font-bold text-white transition-all duration-300 ease-out sm:w-auto hover:scale-110 rounded-xl"
+                role="button">
+                <h1 class="text-3xl font-semibold tracking-tighter uppercase duration-200 ease-in-out">
+                    Nick
+                    Dillon</h1>
+            </RouterLink>
+        </div>
 
         <Socials />
     </nav>
