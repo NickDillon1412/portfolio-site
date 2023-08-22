8342ade98290e66fa54f8c0907fd4a05622a9120
Nick Dillon <nickhds@gmail.com>
2023-08-22 17:11:28 -0500

    Fixing bg issue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,9 @@
 <template>
     <nav
         class="fixed z-10 flex items-center justify-between w-full px-3.5 py-3 sm:py-5 border-b sm:px-10 border-slate-700 backdrop-blur-2xl text-slate-50">
-        <RouterLink to="/" name="Home" class="backdrop-blur-lg">
+        <RouterLink to="/" name="Home">
             <h1
-                class="text-3xl font-semibold tracking-tighter uppercase duration-300 ease-in-out bg-transparent hover:text-pink-400 hover:scale-110">
+                class="text-3xl font-semibold tracking-tighter uppercase duration-300 ease-in-out hover:text-pink-400 hover:scale-110">
                 Nick
                 Dillon</h1>
         </RouterLink>
