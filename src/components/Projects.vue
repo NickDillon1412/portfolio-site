057c3795158eb26789867a25ce74e8d167611686
Nick Dillon <nickhds@gmail.com>
2023-08-23 13:07:55 -0500

    Updated tech pill size on desktop

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -10,7 +10,7 @@
 							project.title }}</h5>
 						<ul v-for="tech in project.technologies" class="inline-block pt-3">
 							<li
-								class="px-2 py-0.5 mr-2 text-xs font-semibold text-pink-600 bg-pink-200 rounded-full shadow-sm shadow-pink-200/75">
+								class="px-2 sm:px-2.5 py-0.5 sm:py-[.8px] mr-2 text-xs sm:text-sm font-semibold text-pink-600 bg-pink-200 rounded-full shadow-sm shadow-pink-200/75">
 								{{ tech }}</li>
 						</ul>
 					</div>
