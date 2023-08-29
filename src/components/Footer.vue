b1ae393786cba08d2cfe9cfdcd702ac7ccac56c0
Nick Dillon <nickhds@gmail.com>
2023-08-29 11:24:52 -0500

    Changed button bg color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,7 +10,7 @@
                             class="absolute transition-all duration-1000 bg-pink-500 -inset-px rounded-xl blur-lg group-hover/button:opacity-100 group-hover/button:-inset-1 group-hover/button:duration-200 animate-pulse">
                         </div>
                         <a href="mailto:nickhds@gmail.com" target="_blank"
-                            class="relative inline-flex items-center justify-center w-full px-4 py-2 text-lg font-bold text-white transition-all duration-300 ease-in-out bg-pink-500 sm:w-auto hover:-rotate-3 hover:scale-110 rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+                            class="relative inline-flex items-center justify-center w-full px-4 py-2 text-lg font-bold text-white transition-all duration-300 ease-in-out bg-pink-400 sm:w-auto hover:-rotate-3 hover:scale-110 rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
                             role="button">Send me a message!</a>
                     </div>
                     <span class="hidden sm:inline-block">|</span>
