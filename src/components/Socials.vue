baece3697a0e333b950ad72826c9cbb25bbc6bb1
Nick Dillon <nickhds@gmail.com>
2023-09-07 10:13:19 -0500

    Updated to use X Twitter logo

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -2,24 +2,24 @@
 	<div class="flex space-x-3">
 		<div class="ml-0 duration-300 ease-in-out border-b-2 border-transparent hover:border-b-2 hover:scale-125">
 			<a href="https://twitter.com/NickDillon1412" target="_blank">
-				<TwitterIcon class="duration-300 ease-in-out w-7 h-7 sm:h-8 sm:w-8 hover:text-pink-500" />
+				<XIcon class="w-6 h-6 duration-300 ease-in-out hover:text-pink-500" />
 			</a>
 		</div>
 		<div class="ml-0 duration-300 ease-in-out border-b-2 border-transparent hover:border-b-2 hover:scale-125">
 			<a href="https://github.com/NickDillon1412" target="_blank">
-				<GitHubIcon class="duration-300 ease-in-out w-7 h-7 sm:h-8 sm:w-8 hover:text-pink-500" />
+				<GitHubIcon class="w-6 h-6 duration-300 ease-in-out hover:text-pink-500" />
 			</a>
 		</div>
 		<div class="ml-0 duration-300 ease-in-out border-b-2 border-transparent hover:border-b-2 hover:scale-125">
 			<a href="https://www.linkedin.com/in/nickdillon12/" target="_blank">
-				<LinkedInIcon class="duration-300 ease-in-out w-7 h-7 sm:h-8 sm:w-8 hover:text-pink-500" />
+				<LinkedInIcon class="w-6 h-6 duration-300 ease-in-out hover:text-pink-500" />
 			</a>
 		</div>
 	</div>
 </template>
 
 <script setup>
-import GitHubIcon from "~icons/mdi/github";
-import LinkedInIcon from "~icons/mdi/linkedin";
-import TwitterIcon from "~icons/mdi/twitter";
+import GitHubIcon from "~icons/simple-icons/github";
+import LinkedInIcon from "~icons/simple-icons/linkedin";
+import XIcon from "~icons/simple-icons/x";
 </script>
