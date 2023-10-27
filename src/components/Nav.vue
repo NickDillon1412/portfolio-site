752b98990206616e29ed1889d253ffc7b2488447
Nick Dillon <nickhds@gmail.com>
2023-10-27 12:02:00 -0500

    Added max width for xl screens

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,15 @@
 <template>
-    <nav
-        class="fixed z-10 flex items-center justify-between w-full px-3.5 py-3 sm:py-3.5 border-b sm:px-10 border-slate-700 backdrop-blur-2xl text-slate-50">
-        <RouterLink to="/" name="Home">
-            <h1
-                class="text-3xl font-semibold tracking-tighter uppercase duration-300 ease-in-out hover:text-pink-500 hover:scale-110">
-                Nick
-                Dillon</h1>
-        </RouterLink>
-
-        <Socials />
-    </nav>
+    <div class="fixed w-full z-10 px-3.5 py-3 sm:py-3.5 border-b sm:px-10 border-slate-700 backdrop-blur-2xl text-slate-50">
+        <nav class="flex items-center justify-between mx-auto xl:max-w-5xl">
+            <RouterLink to="/" name="Home">
+                <h1
+                    class="text-3xl font-semibold tracking-tighter uppercase duration-300 ease-in-out hover:text-pink-500 hover:scale-110">
+                    Nick
+                    Dillon</h1>
+            </RouterLink>
+            <Socials />
+        </nav>
+    </div>
 </template>
 
 <script setup>
